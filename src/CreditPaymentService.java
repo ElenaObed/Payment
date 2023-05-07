fbc33ea734c282ba31915e42b99f4935bd5f442f
Elena Obed <elena_sergeevna137@mail.ru>
2023-05-07 16:31:18 +0300

    payment1

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,9 +1,9 @@
 public class CreditPaymentService {
-    public int calculate(int sumCredit,int years,double percentCredit) {
-        double monthPercent = percentCredit / (12*100);
+    public int calculate(int sumCredit, int years, double percentCredit) {
+        double monthPercent = percentCredit / (12 * 100);
         double sumMonth = years * 12;
 
-        double monthPayment = sumCredit * monthPercent * Math.pow(1+monthPercent, sumMonth) / (Math.pow(1+monthPercent, sumMonth) - 1);
+        double monthPayment = sumCredit * monthPercent * Math.pow(1 + monthPercent, sumMonth) / (Math.pow(1 + monthPercent, sumMonth) - 1);
 
         return (int) monthPayment;
     }
